d622000f4faa52984af9fff10ea269a5ba1ec435
Tomas Molina <tamolinamendoza@gmail.com>
2016-06-20 18:26:45 +0000

    Done.

---
+++ trip.rb
@@ -0,0 +1,11 @@
+#Code here!
+puts "Where would you like to stay?"
+stay = gets.chomp
+puts "What site would you like to visit?"
+sites = gets.chomp
+puts "What food would you like to eat?"
+food = gets.chomp
+puts "How many nights do you plan to stay?"
+nights = gets.chomp
+
+puts "You will be staying in #{stay}, visiting #{sites} and eating #{food} during your #{nights} nights of stay in NYC."
